ef6da186aed8e5eabd7036e6934c5903df2fbfa7
Tednih <dimassted@gmail.com>
2023-09-28 08:15:25 +0700

    fix dynamic routes

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"DimasGo/config"
 	"DimasGo/routes"
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatalf("Error loading .env file: %v", err)
+	// }
 
 	config.InitDB()
 
